fadfff2c1847b100b0c1ed4869b1648e06ae370c
tanvircr7 <tanvir.hasan.cr7@gmail.com>
2022-12-24 12:58:32 +0600

    gasLane needs to be passed as string on helper-hardhat-config to resolve previous invalid arrayify value error

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,7 +5,7 @@ const networkConfig = {
         name: "goerli",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane: 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15, //150 gwei key hash gas lane
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", //150 gwei key hash gas lane
         subscriptionId: "0", // we'll update it when creating new subscription
         callbackGasLimit: "500000",
         interval: "30",
@@ -13,7 +13,7 @@ const networkConfig = {
     31337: {
         name: "hardhat",
         entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane: 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15, // don't care
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // don't care
         callbackGasLimit: "500000",
         interval: "30",
     },
